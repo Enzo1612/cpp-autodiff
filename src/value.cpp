d0f3a12eacae5a3469416e0c35c2fcf5e7c6f0cc
Enzo1612 <enzonicaise.1612@gmail.com>
2026-04-18 13:45:15 +0200

    Rebuilt this project from scratch after studying the underlying concepts again. feat: implement composite autograd operators and memory-safe graph wiring
    - Refactor Value node primitive to utilize std::shared_ptr for automated graph memory management.
    - Implement global operator overloading (+, -, *, /) with lambda-based backward pass enclosures.
    - Resolve gradient accumulation bug by enforcing addition assignment (+=) in local derivative calculations.
    - Implement composite division operator (a * b^-1) leveraging existing valuePow and operator* primitives.

--- src/value.cpp
+++ src/value.cpp
@@ -1,20 +1,69 @@
-#include "../include/autodiff/value.hpp"
+#include "value.hpp"
 
-Value *Value::operator+(Value &other)
+#include <cmath>
+
+ValuePtr operator+(ValuePtr parent1, ValuePtr parent2)
 {
-    double result = this->m_data + other.m_data;
+    ValuePtr out = std::make_shared<Value>(parent1->_data + parent2->_data);
 
-    // new to allocate on the heap, before I was creating dangling memory, if parents get deleted, children points to garbage
-    Value *out = new Value(result, {this, &other}, "+");
+    out->_prev = {parent1, parent2};
+    out->_op = "+";
+
+    out->_backward = [parent1, parent2, out]()
+    {
+        parent1->_grad += out->_grad * 1.0;
+        parent2->_grad += out->_grad * 1.0;
+    };
 
     return out;
 }
 
-Value *Value::operator*(Value &other)
+ValuePtr operator-(ValuePtr parent1, ValuePtr parent2)
 {
-    double result = this->m_data * other.m_data;
+    ValuePtr out = std::make_shared<Value>(parent1->_data - parent2->_data);
 
-    Value *out = new Value(result, {this, &other}, "*");
+    out->_prev = {parent1, parent2};
+    out->_op = "-";
+
+    out->_backward = [parent1, parent2, out]()
+    {
+        parent1->_grad += out->_grad * 1.0;
+        parent2->_grad += out->_grad * -1.0;
+    };
+    return out;
+}
+
+ValuePtr operator*(ValuePtr parent1, ValuePtr parent2)
+{
+    ValuePtr out = std::make_shared<Value>(parent1->_data * parent2->_data);
+
+    out->_prev = {parent1, parent2};
+    out->_op = "*";
+
+    out->_backward = [parent1, parent2, out]()
+    {
+        parent1->_grad += out->_grad * parent2->_data;
+        parent2->_grad += out->_grad * parent1->_data;
+    };
+    return out;
+}
+
+ValuePtr operator/(ValuePtr parent1, ValuePtr parent2)
+{
+    return parent1 * valuePow(parent2, -1.0);
+}
+
+ValuePtr valuePow(ValuePtr parent1, double exp)
+{
+    ValuePtr out = std::make_shared<Value>(pow(parent1->_data, exp));
+
+    out->_prev = {parent1};
+    out->_op = "^";
+
+    out->_backward = [parent1, exp, out]()
+    {
+        parent1->_grad += pow(parent1->_data, exp - 1) * exp * out->_grad;
+    };
 
     return out;
 }
---
+++ src/value.hpp
@@ -0,0 +1,30 @@
+#ifndef VALUE_HPP
+#define VALUE_HPP
+
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <string>
+
+class Value
+{
+public:
+    Value(double data) : _data(data), _op(""), _grad(0.0), _backward([]() {}) {}
+
+    double _data;
+    std::string _op;
+    double _grad;
+    std::vector<std::shared_ptr<Value>> _prev;
+    std::function<void()> _backward;
+};
+
+using ValuePtr = std::shared_ptr<Value>;
+
+ValuePtr operator+(ValuePtr parent1, ValuePtr parent2);
+ValuePtr operator-(ValuePtr parent1, ValuePtr parent2);
+ValuePtr operator*(ValuePtr parent1, ValuePtr parent2);
+ValuePtr operator/(ValuePtr parent1, ValuePtr parent2);
+ValuePtr valuePow(ValuePtr parent, double exponent);
+
+#endif
